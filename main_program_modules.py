e891c61a746c227e47089aa55cacafa2687d43a6
Miki <miqeelcn@gmail.com>
2019-03-29 17:49:11 +0100

    Readaption to complete function of module 4

--- main_program_modules.py
+++ main_program_modules.py
@@ -159,9 +159,9 @@ elif argsdict['module'] == 3:
     argsdict['module'] = None
 
 elif argsdict['module'] == 4:
-#    u, argsdict = frames_selector.frame_selector(u, argsdict)
+    u, argsdict = frames_selector.frame_selector(u, argsdict)
     #frames_selector.distance_comparison(argsdict)
-    frames_selector.distance_cutoff(argsdict)
+#    frames_selector.distance_cutoff(argsdict)
     argsdict['module'] = None
 
 elif argsdict['module'] == 5:
